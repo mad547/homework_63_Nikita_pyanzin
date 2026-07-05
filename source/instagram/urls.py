2a8f1be8de96097227e01fb6b1b6ec664bdfa8a2
mad547 <97340914+mad547@users.noreply.github.com>
2026-07-05 21:22:01 +0600

    исправил главный urls.py

--- source/instagram/urls.py
+++ source/instagram/urls.py
@@ -14,19 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from posts.views import (
-    IndexView, PostDetailView, PostCreateView,
-    PostDeleteView, LikeView, CommentCreateView
-)
-
-app_name = 'posts'
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('post/create/', PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    path('post/<int:pk>/like/', LikeView.as_view(), name='like'),
-    path('post/<int:pk>/comment/', CommentCreateView.as_view(), name='comment_create'),
-]
+    path('admin/', admin.site.urls),
+    path('', include('posts.urls')),
+    path('accounts/', include('accounts.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
